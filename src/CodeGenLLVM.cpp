d8e935aa147d52aa75534f00815d889af4f31276
Paweł Sołtysiak <soltys@uznam.org>
2013-04-28 11:32:14 +0200

    llvm basic code generation

--- src/CodeGenLLVM.cpp
+++ src/CodeGenLLVM.cpp
@@ -10,12 +10,35 @@
 #include "parser.hh"
 #include <iostream>
 #include <string>
+
+#include <llvm/PassManagers.h>
+#include <llvm/DefaultPasses.h>
+#include <llvm/Instructions.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/Support/raw_os_ostream.h>
 using namespace llvm;
 using namespace std;
 namespace PSLang {
 typedef PSLang::Parser::token token;
 CodeGenLLVM::CodeGenLLVM(std::string fileName) :
-		builder(getGlobalContext()) {
+		outputStream(fileName, std::ofstream::out), builder(getGlobalContext()), mainBlock(
+				nullptr) {
+
+	theModule = new Module("pslang module", getGlobalContext());
+
+	theFPM = new FunctionPassManager(theModule);
+	// Provide basic AliasAnalysis support for GVN.
+	theFPM->add(createBasicAliasAnalysisPass());
+	// Do simple "peephole" optimizations and bit-twiddling optzns.
+	theFPM->add(createInstructionCombiningPass());
+	// Reassociate expressions.
+	theFPM->add(createReassociatePass());
+	// Eliminate Common SubExpressions.
+	theFPM->add(createGVNPass());
+	// Simplify the control flow graph (deleting unreachable blocks, etc).
+	theFPM->add(createCFGSimplificationPass());
+//theFPM->add(createPrintModulePass(&outs()));
+	theFPM->doInitialization();
 
 }
 
@@ -23,13 +46,37 @@ CodeGenLLVM::~CodeGenLLVM() {
 	// TODO Auto-generated destructor stub
 }
 
-llvm::Value* CodeGenLLVM::getVariable(const std::string& name)  {
+static Type *typeOf(const NIdentifier& type) {
+	if (type.name.compare("int") == 0) {
+		return Type::getInt64Ty(getGlobalContext());
+	} else if (type.name.compare("double") == 0) {
+		return Type::getDoubleTy(getGlobalContext());
+	}
+	return Type::getVoidTy(getGlobalContext());
+}
+
+llvm::Value* CodeGenLLVM::getVariable(const std::string& name) {
 	return namedValues[name];
 }
 
 void CodeGenLLVM::generateCode(NBlock& root) {
 	std::cout << "Generating code...\n";
 
+	/* Create the top level interpreter function to call as entry */
+	vector<Type*> argTypes;
+	FunctionType *ftype = FunctionType::get(Type::getVoidTy(getGlobalContext()),
+			argTypes, false);
+	Function* mainFunction = Function::Create(ftype,
+			GlobalValue::InternalLinkage, "main", theModule);
+	mainBlock = BasicBlock::Create(getGlobalContext(), "entry", mainFunction,
+			0);
+
+	root.codeGen(*this);
+
+	ReturnInst::Create(getGlobalContext(), mainBlock);
+	//raw_os_ostream rawStream(outputStream);
+//	WriteBitcodeToFile(theModule,rawStream);
+	theModule->dump();
 	std::cout << "Code is generated.\n";
 
 }
@@ -46,7 +93,8 @@ Value* NArrayIdentifier::codeGen(CodeGenLLVM& context) {
 }
 Value* NIdentifier::codeGen(CodeGenLLVM& context) {
 	Value *V = context.getVariable(name);
-	return V ? V : nullptr;
+
+	return new LoadInst(V, "", false, context.mainBlock);
 }
 
 Value* NMethodCall::codeGen(CodeGenLLVM& context) {
@@ -54,38 +102,56 @@ Value* NMethodCall::codeGen(CodeGenLLVM& context) {
 }
 
 Value* NBinaryOperator::codeGen(CodeGenLLVM& context) {
-	Value *L = lhs.codeGen(context);
-	Value *R = rhs.codeGen(context);
-	if (L == nullptr || R == nullptr)
-		return nullptr;
-
+	std::cout << "Creating binary operation " << std::endl;
+	Instruction::BinaryOps instr;
 	switch (op) {
 	case token::TPLUS:
-		return context.builder.CreateFAdd(L, R, "addtmp");
+		instr = Instruction::Add;
+		break;
 	case token::TMINUS:
-		return context.builder.CreateFSub(L, R, "subtmp");
+		instr = Instruction::Sub;
+		break;
 	case token::TMUL:
-		return context.builder.CreateFMul(L, R, "multmp");
-	case token::TCLT:
-		L = context.builder.CreateFCmpULT(L, R, "cmptmp");
-		// Convert bool 0/1 to double 0.0 or 1.0
-		return context.builder.CreateUIToFP(L,
-				Type::getDoubleTy(getGlobalContext()), "booltmp");
+		instr = Instruction::Mul;
+		break;
+	case token::TDIV:
+		instr = Instruction::SDiv;
+		break;
 	default:
-		return (nullptr);
+		return nullptr;
 	}
+
+	return BinaryOperator::Create(instr, lhs.codeGen(context),
+			rhs.codeGen(context), "", context.mainBlock);
 }
 
 Value* NAssignment::codeGen(CodeGenLLVM& context) {
-	return (nullptr);
+	std::cout << "Creating assignment for " << lhs.name << std::endl;
+	if (context.namedValues.find(lhs.name) == context.namedValues.end()) {
+		std::cerr << "undeclared variable " << lhs.name << std::endl;
+		return NULL;
+	}
+	return new StoreInst(rhs.codeGen(context), context.namedValues[lhs.name],
+			false, context.mainBlock);
 }
 
 Value* NBlock::codeGen(CodeGenLLVM& context) {
-	return (nullptr);
+
+	StatementList::const_iterator it;
+	Value *last = NULL;
+	for (it = statements.begin(); it != statements.end(); it++) {
+		std::cout << "Generating code for " << typeid(**it).name() << std::endl;
+		last = (**it).codeGen(context);
+	}
+	std::cout << "Creating block" << std::endl;
+
+	return last;
 }
 
 Value* NExpressionStatement::codeGen(CodeGenLLVM& context) {
-	return (nullptr);
+	std::cout << "Generating code for " << typeid(expression).name()
+			<< std::endl;
+	return expression.codeGen(context);
 }
 
 Value* NArrayDeclaration::codeGen(CodeGenLLVM& context) {
@@ -93,7 +159,16 @@ Value* NArrayDeclaration::codeGen(CodeGenLLVM& context) {
 }
 
 Value* NVariableDeclaration::codeGen(CodeGenLLVM& context) {
-	return (nullptr);
+	std::cout << "Creating variable declaration " << type.name << " " << id.name
+			<< std::endl;
+	AllocaInst *alloc = new AllocaInst(typeOf(type), id.name.c_str(),
+			context.mainBlock);
+	context.namedValues[id.name] = alloc;
+	if (assignmentExpression != NULL) {
+		NAssignment assn(id, *assignmentExpression);
+		assn.codeGen(context);
+	}
+	return alloc;
 }
 
 Value* NFunctionDeclaration::codeGen(CodeGenLLVM& context) {
--- src/CodeGenLLVM.h
+++ src/CodeGenLLVM.h
@@ -10,9 +10,12 @@
 
 #include "CodeGenBase.hpp"
 #include <string>
+#include <memory>
 #include <stack>
 #include <map>
 #include <vector>
+#include <fstream>
+
 #include <llvm/Module.h>
 #include <llvm/Function.h>
 #include <llvm/Type.h>
@@ -26,16 +29,30 @@
 #include <llvm/Assembly/PrintModulePass.h>
 #include <llvm/Support/IRBuilder.h>
 #include <llvm/Support/raw_ostream.h>
-
+#include <llvm/Pass.h>
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/PassManager.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/Target/TargetData.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Support/TargetSelect.h"
+#include <llvm/Support/raw_os_ostream.h>
 namespace PSLang {
 class NBlock;
 class CodeGenLLVM: public PSLang::CodeGenBase {
 
-	llvm::Module *TheModule;
+	std::ofstream outputStream;
 
-	std::map<std::string, llvm::Value*> namedValues;
+	llvm::FunctionPassManager*  theFPM;
+
+
+
 public:
+	llvm::Module *theModule;
 	llvm::IRBuilder<> builder;
+	llvm::BasicBlock* mainBlock;
+	std::map<std::string, llvm::Value*> namedValues;
 	llvm::Value* getVariable(const std::string& name);
 	CodeGenLLVM(std::string fileName);
 	virtual void generateCode(NBlock& root);
